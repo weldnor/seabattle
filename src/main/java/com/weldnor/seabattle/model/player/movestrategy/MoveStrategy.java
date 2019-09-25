b831559a677c8c3c4d2ddfa3124058bff49d9383
Anton Kolesov <kolesov.anton.s@gmail.com>
2019-09-25 20:43:50 +0300

    Add MoveType in makeMove method

--- src/main/java/com/weldnor/seabattle/model/player/movestrategy/MoveStrategy.java
+++ src/main/java/com/weldnor/seabattle/model/player/movestrategy/MoveStrategy.java
@@ -1,8 +1,9 @@
 package com.weldnor.seabattle.model.player.movestrategy;
 
+import com.weldnor.seabattle.model.MoveType;
 import com.weldnor.seabattle.model.map.Map;
 import com.weldnor.seabattle.model.map.Point;
 
 public interface MoveStrategy {
-    Point makeMove(Map playerMap, Map enemyMap);
+    Point makeMove(Map ownMap, Map enemyMap, MoveType moveType);
 }
