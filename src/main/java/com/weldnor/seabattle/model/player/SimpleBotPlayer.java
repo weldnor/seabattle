22f353a0bb5fa41b45df7990ee5c855fdc31a5b4
Anton Kolesov <kolesov.anton.s@gmail.com>
2019-10-05 17:32:02 +0300

    Implement SimpleBotPlayer class

--- src/main/java/com/weldnor/seabattle/model/player/SimpleBotPlayer.java
+++ src/main/java/com/weldnor/seabattle/model/player/SimpleBotPlayer.java
@@ -1,4 +1,38 @@
 package com.weldnor.seabattle.model.player;
 
-public class SimpleBotPlayer implements Player {
+import com.weldnor.seabattle.model.MoveType;
+import com.weldnor.seabattle.model.map.*;
+
+import java.util.List;
+import java.util.Random;
+
+public class SimpleBotPlayer implements BotPlayer {
+
+    private Random random = new Random();
+
+    @Override
+    public Point makeMove(Map ownMap, Map enemyMap, MoveType moveType) {
+        if (moveType == MoveType.Normal) {
+            int x = random.nextInt(enemyMap.size());
+            int y = random.nextInt(enemyMap.size());
+            return new Point(x, y);
+        }
+        if (moveType == MoveType.SelectMine) {
+            List<Cell> mines = ownMap.findCells(CellType.Mine);
+            Cell mine = mines.get(random.nextInt(mines.size()));
+            return mine.getPoint();
+        }
+        if (moveType == MoveType.SelectShip) {
+            List<Cell> shipCells = ownMap.findCells(CellType.Ship);
+
+            for (Cell shipCell : shipCells)
+                if (!shipCell.isDestroyed()) return shipCell.getPoint();
+        }
+        return null;
+    }
+
+    @Override
+    public Map makeMap() {
+        return MapUtils.loadMapFromResource("/maps/1.txt");
+    }
 }
