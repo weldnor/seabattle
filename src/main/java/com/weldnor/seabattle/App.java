e4583bac1d62504705cdc1108594c879b624e025
Anton Kolesov <kolesov.anton.s@gmail.com>
2019-10-26 10:54:27 +0300

    Add simple console interface

--- src/main/java/com/weldnor/seabattle/App.java
+++ src/main/java/com/weldnor/seabattle/App.java
@@ -2,6 +2,8 @@ package com.weldnor.seabattle;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        Engine engine = new Engine();
+        engine.init();
+        engine.start();
     }
 }
---
+++ src/main/java/com/weldnor/seabattle/Engine.java
@@ -0,0 +1,102 @@
+package com.weldnor.seabattle;
+
+import com.weldnor.seabattle.model.Game;
+import com.weldnor.seabattle.model.map.MapUtils;
+import com.weldnor.seabattle.model.map.Point;
+import com.weldnor.seabattle.model.player.BotPlayer;
+import com.weldnor.seabattle.model.player.HumanPlayer;
+import com.weldnor.seabattle.model.player.Player;
+import com.weldnor.seabattle.model.player.SimpleBotPlayer;
+
+import java.util.Scanner;
+
+public class Engine {
+    private Scanner scanner = new Scanner(System.in);
+    private Game game;
+    private Player firstPlayer;
+    private Player secondPlayer;
+
+    private int mode;
+
+    public void init() {
+        System.out.println("hi!\n" +
+                "[1] player vs player\n" +
+                "[2] player vs bot\n"
+        );
+
+        int mode = scanner.nextInt();
+        this.mode = mode;
+
+        switch (mode) {
+            case 1:
+                firstPlayer = new HumanPlayer();
+                secondPlayer = new HumanPlayer();
+                break;
+            case 2:
+                firstPlayer = new HumanPlayer();
+                secondPlayer = new SimpleBotPlayer();
+                break;
+        }
+
+        game = new Game(firstPlayer, secondPlayer);
+
+        switch (mode) {
+            case 1:
+                game.setFirstPlayerMap(MapUtils.loadMapFromConsole());
+                game.setSecondPlayerMap(MapUtils.loadMapFromConsole());
+                break;
+            case 2:
+                game.setFirstPlayerMap(MapUtils.loadMapFromConsole());
+                game.setSecondPlayerMap(((BotPlayer) secondPlayer).makeMap());
+        }
+    }
+
+    public void start() {
+        int tick = 0;
+        while (!game.isEnd()) {
+
+
+            System.out.println(tick);
+            tick++;
+
+            if (game.getCurrentPlayer() instanceof BotPlayer) {
+                game.makeMoveByBot();
+
+            } else {
+                switch (game.getCurrentMoveType()) {
+                    case Normal:
+                        System.out.println("Normal");
+                        break;
+                    case SelectShip:
+                        System.out.println("Select Ship");
+                        break;
+                    case SelectMine:
+                        System.out.println("Select Mine");
+                        break;
+                }
+
+                String[] ownMapLines = game.getCurrentPlayerMap()
+                        .toString()
+                        .split("\n");
+
+                String[] enemyMapLines = MapUtils.reverse(game.getCurrentEnemyMap())
+                        .toString()
+                        .split("\n");
+
+                for (int i = 0; i < 10; i++) {
+                    System.out.print(ownMapLines[i]);
+                    System.out.print("   ");
+                    System.out.println(enemyMapLines[i]);
+                }
+
+                System.out.println("enter x, y:");
+
+                int x = scanner.nextInt();
+                int y = scanner.nextInt();
+
+                game.makeMoveByHuman(new Point(x, y));
+            }
+        }
+    }
+
+}
