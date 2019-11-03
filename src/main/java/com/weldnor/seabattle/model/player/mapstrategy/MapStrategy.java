0ec7558bf7d18fbee2a79175dc4a888d5fad326c
Anton Kolesov <kolesov.anton.s@gmail.com>
2019-11-03 15:59:16 +0300

    Delete StrategyBotPlayer.java

--- src/main/java/com/weldnor/seabattle/model/player/mapstrategy/MapStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.weldnor.seabattle.model.player.mapstrategy;
-
-import com.weldnor.seabattle.model.map.GameMap;
-
-public interface MapStrategy {
-    GameMap makeMap();
-}
--- src/main/java/com/weldnor/seabattle/model/player/movestrategy/MoveStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.weldnor.seabattle.model.player.movestrategy;
-
-import com.weldnor.seabattle.model.MoveType;
-import com.weldnor.seabattle.model.map.GameMap;
-import com.weldnor.seabattle.model.map.Point;
-
-public interface MoveStrategy {
-    Point makeMove(GameMap ownMap, GameMap enemyMap, MoveType moveType);
-}
