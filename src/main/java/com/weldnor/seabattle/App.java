844d1384e20b906e2faf93dd85db0dc04abf9fa3
Anton Kolesov <kolesov.anton.s@gmail.com>
2019-11-05 19:43:09 +0300

    add support for load/save game to file
    – Update Engine class: add loadGame, saveGame methods;
    – Add Chalk lib for color text output;
    - Add empty save.json;

--- src/main/java/com/weldnor/seabattle/App.java
+++ src/main/java/com/weldnor/seabattle/App.java
@@ -3,7 +3,6 @@ package com.weldnor.seabattle;
 public class App {
     public static void main(String[] args) {
         Engine engine = new Engine();
-        engine.init();
         engine.start();
     }
 }
--- src/main/java/com/weldnor/seabattle/Engine.java
+++ src/main/java/com/weldnor/seabattle/Engine.java
@@ -1,101 +1,208 @@
 package com.weldnor.seabattle;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.github.tomaslanger.chalk.Chalk;
 import com.weldnor.seabattle.model.Game;
-import com.weldnor.seabattle.model.map.GameMapUtils;
-import com.weldnor.seabattle.model.map.Point;
-import com.weldnor.seabattle.model.player.BotPlayer;
-import com.weldnor.seabattle.model.player.HumanPlayer;
-import com.weldnor.seabattle.model.player.Player;
-import com.weldnor.seabattle.model.player.SimpleBotPlayer;
+import com.weldnor.seabattle.model.GameState;
+import com.weldnor.seabattle.model.map.*;
+import com.weldnor.seabattle.model.player.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Engine {
+    private final String savePath = "/save.json";
+
     private Scanner scanner = new Scanner(System.in);
-    private Game game;
-    private Player firstPlayer;
-    private Player secondPlayer;
-
-    private int mode;
-
-    public void init() {
-        System.out.println("hi!\n" +
-                "[1] player vs player\n" +
-                "[2] player vs bot\n"
-        );
-
-        int mode = scanner.nextInt();
-        this.mode = mode;
-
-        switch (mode) {
-            case 1:
-                firstPlayer = new HumanPlayer();
-                secondPlayer = new HumanPlayer();
-                break;
-            case 2:
-                firstPlayer = new HumanPlayer();
-                secondPlayer = new SimpleBotPlayer();
-                break;
-        }
-
-        game = new Game(firstPlayer, secondPlayer);
-
-        switch (mode) {
-            case 1:
-                game.setFirstPlayerMap(GameMapUtils.loadMapFromConsole());
-                game.setSecondPlayerMap(GameMapUtils.loadMapFromConsole());
-                break;
-            case 2:
-                game.setFirstPlayerMap(GameMapUtils.loadMapFromConsole());
-                game.setSecondPlayerMap(((BotPlayer) secondPlayer).makeMap());
-        }
-    }
+    private Game game = new Game();
 
     public void start() {
-        int tick = 0;
+        File saveFile = new File(this.getClass()
+                .getResource(savePath)
+                .getPath());
+
+        if (saveFile.exists() && saveFile.length() > 0) {
+            System.out.println("load game? y/n");
+            if (scanner.nextLine().equals("y")) {
+                loadGame();
+            } else {
+                createNewGame();
+            }
+        } else {
+            createNewGame();
+        }
+
         while (!game.isEnd()) {
+            tick();
+        }
+        System.out.println("END.");
+    }
 
-
-            System.out.println(tick);
-            tick++;
+    private void tick() {
+        while (!game.isEnd()) {
 
             if (game.getCurrentPlayer() instanceof BotPlayer) {
                 game.makeMoveByBot();
 
             } else {
+                System.out.println("own map:");
+                printMap(game.getCurrentPlayerMap());
+
+                System.out.println("enemy map:");
+                printMap(GameMapUtils.reverse(game.getCurrentEnemyMap()));
+
+
+                Point point = null;
+
                 switch (game.getCurrentMoveType()) {
                     case Normal:
-                        System.out.println("Normal");
+                        System.out.println("select cell:");
+                        point = selectCell();
                         break;
                     case SelectShip:
-                        System.out.println("Select Ship");
+                        System.out.println("select ship cell:");
+                        point = selectShipCell();
                         break;
                     case SelectMine:
-                        System.out.println("Select Mine");
+                        System.out.println("select mine cell:");
+                        point = selectMineCell();
                         break;
+
                 }
-
-                String[] ownMapLines = game.getCurrentPlayerMap()
-                        .toString()
-                        .split("\n");
-
-                String[] enemyMapLines = GameMapUtils.reverse(game.getCurrentEnemyMap())
-                        .toString()
-                        .split("\n");
-
-                for (int i = 0; i < 10; i++) {
-                    System.out.print(ownMapLines[i]);
-                    System.out.print("   ");
-                    System.out.println(enemyMapLines[i]);
-                }
-
-                System.out.println("enter x, y:");
-
-                int x = scanner.nextInt();
-                int y = scanner.nextInt();
-
-                game.makeMoveByHuman(new Point(x, y));
+                game.makeMoveByHuman(point);
             }
+            saveGame();
+        }
+    }
+
+    private void printMap(GameMap map) {
+        for (int i = 0; i < map.size(); i++) {
+            for (int j = 0; j < map.size(); j++) {
+                Cell cell = map.getCell(i, j);
+
+                //water
+                if (cell.getType() == CellType.Water && !cell.isDestroyed())
+                    System.out.print(Chalk.on(".").blue());
+                if (cell.getType() == CellType.Water && cell.isDestroyed())
+                    System.out.print(Chalk.on(".").red());
+
+                //ship
+                if (cell.getType() == CellType.Ship && !cell.isDestroyed())
+                    System.out.print(Chalk.on("#").yellow());
+                if (cell.getType() == CellType.Ship && cell.isDestroyed())
+                    System.out.print(Chalk.on("#").red());
+
+                //mine
+                if (cell.getType() == CellType.Mine && !cell.isDestroyed())
+                    System.out.print(Chalk.on("*").green());
+                if (cell.getType() == CellType.Mine && cell.isDestroyed())
+                    System.out.print(Chalk.on("*").red());
+
+                //minesweeper
+                if (cell.getType() == CellType.Minesweeper && !cell.isDestroyed())
+                    System.out.print(Chalk.on("%").white());
+                if (cell.getType() == CellType.Minesweeper && cell.isDestroyed())
+                    System.out.print(Chalk.on("%").red());
+
+                //hidden
+                if (cell.getType() == CellType.Hidden)
+                    System.out.print(Chalk.on(".").gray());
+            }
+            System.out.println();
+        }
+    }
+
+    private Point selectCell() {
+        GameMap map = game.getCurrentPlayerMap();
+
+        int x = -1;
+        int y = -1;
+
+        while (0 > x || x >= map.size() || 0 > y || y >= map.size()) {
+            System.out.println("enter x:");
+            x = scanner.nextInt();
+            System.out.println("enter y:");
+            y = scanner.nextInt();
+        }
+
+        return new Point(x, y);
+    }
+
+    private Point selectShipCell() {
+        GameMap map = game.getCurrentPlayerMap();
+
+        while (true) {
+            Point p = selectCell();
+            Cell c = map.getCell(p);
+            if (c.getType() == CellType.Ship && !c.isDestroyed())
+                return p;
+        }
+    }
+
+    private Point selectMineCell() {
+        GameMap map = game.getCurrentPlayerMap();
+
+        while (true) {
+            Point p = selectCell();
+            Cell c = map.getCell(p);
+            if (c.getType() == CellType.Mine && !c.isDestroyed())
+                return p;
+        }
+    }
+
+    private void createNewGame() {
+        game.setFirstPlayer(new HumanPlayer());
+        game.setSecondPlayer(new SimpleBotPlayer());
+
+        game.setFirstPlayerMap(GameMapUtils.loadMapFromConsole());
+        game.setSecondPlayerMap(((BotPlayer) game.getSecondPlayer()).makeMap());
+    }
+
+    public void saveGame() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        SimpleModule module = new SimpleModule("my_module");
+        module.addSerializer(Player.class, new PlayerSerializer(Player.class));
+        objectMapper.registerModule(module);
+
+        File saveFile = new File(this.getClass().getResource(savePath).getPath());
+
+        try {
+            objectMapper.writeValue(
+                    saveFile,
+                    game.getState()
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadGame() {
+        Scanner scanner = new Scanner(Engine.class.getResourceAsStream(savePath));
+        StringBuilder builder = new StringBuilder();
+
+        while (scanner.hasNextLine()) {
+            builder.append(scanner.nextLine()).append("\n");
+        }
+        String json = builder.toString();
+
+        System.out.println(json);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        SimpleModule module = new SimpleModule("my_module");
+        module.addDeserializer(Player.class, new PlayerDeserializer(Player.class));
+        objectMapper.registerModule(module);
+
+        try {
+            GameState state = objectMapper.readValue(json, GameState.class);
+            System.out.println(state);
+            game.setState(state);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
         }
     }
 
